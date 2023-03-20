b4bd0a3e595c0e17c482d60f565b64392b0160fa
skabeo <spencerokyere95@gmail.com>
2023-03-20 14:29:19 +0000

    Add functionality to page

--- script.js
+++ script.js
@@ -1,22 +1,74 @@
-const btn = document.getElementById('submit');
-let title, author;
+const bookList = document.querySelector('#books-list');
+const bookForm = document.querySelector('#book-form');
 
-btn.addEventListener("click", ()=> {
-    title = document.querySelector('#title').value;
-    author = document.querySelector('#author').value;
+let books;
 
-    var books = [];
-    books.push(
+let savedBooks = JSON.parse(localStorage.getItem('book'));
+if (Array.isArray(savedBooks)) {
+    books = savedBooks;
+} else {
+    books = [
         {
-            title: title,
-            author: author
-        }
-    );
+            title: 'book 1',
+            author: 'author 1',
+        },
+        {
+            title: 'book 2',
+            author: 'author 2',
+        },
+    ]
+};
 
-    localStorage.setItem("books", books);
-    var ans = localStorage.getItem("books");
-    console.log(ans.title);
-    console.log(ans.author);
+render();
+
+bookForm.addEventListener('submit', addBook);
+bookList.addEventListener('click', remove);
+
+function render() {
+    bookList.innerHTML = '';
+    books.forEach((book, index) => {
+        const li = document.createElement('li');
+        const line = document.createElement('hr');
+        line.classList.add('line');
+        const title = document.createElement('span');
+        title.classList.add('display');
+        const author = document.createElement('span');
+        author.classList.add('display');
+        const btn = document.createElement('button');
+        btn.classList.add('display');
+        btn.innerHTML = `Remove`;
+        btn.id = `${index}`;
+        title.innerHTML = `${book.title}`
+        author.innerHTML = `${book.author}`;
+        li.appendChild(title);
+        li.appendChild(author);
+        li.appendChild(btn);
+        bookList.appendChild(li);
+        bookList.appendChild(line);
+    });
+}
 
 
-});
+function addBook(event) {
+    event.preventDefault();
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    books.push({ title, author });
+    render();
+    bookForm.reset();
+    saveBook();
+}
+
+function remove(event) {
+    console.log(event);
+    if (event.target.tagName === 'BUTTON') {
+        const index = event.target.id;
+        books.splice(index, 1);
+        render();
+    }
+    saveBook();
+}
+
+function saveBook() {
+    localStorage.setItem('book', JSON.stringify(books));
+}
