af82404ff49821b60c70e262e3ea67289206abac
Ahmad <ehmaddd@gmail.com>
2023-03-21 12:45:29 +0500

    Add all files

--- script.js
+++ script.js
@@ -1,72 +1,100 @@
-const bookList = document.querySelector('#books-list');
-const bookForm = document.querySelector('#book-form');
-
-let books;
-
-const savedBooks = JSON.parse(localStorage.getItem('book'));
-if (Array.isArray(savedBooks)) {
-  books = savedBooks;
-} else {
-  books = [
-    {
-      title: 'book 1',
-      author: 'author 1',
-    },
-    {
-      title: 'book 2',
-      author: 'author 2',
-    },
-  ];
-}
-
-function render() {
-  bookList.innerHTML = '';
-  books.forEach((book, index) => {
-    const li = document.createElement('li');
-    const line = document.createElement('hr');
-    line.classList.add('line');
-    const title = document.createElement('span');
-    title.classList.add('display');
-    const author = document.createElement('span');
-    author.classList.add('display');
-    const btn = document.createElement('button');
-    btn.classList.add('display');
-    btn.innerHTML = 'Remove';
-    btn.id = `${index}`;
-    title.innerHTML = `${book.title}`;
-    author.innerHTML = `${book.author}`;
-    li.appendChild(title);
-    li.appendChild(author);
-    li.appendChild(btn);
-    bookList.appendChild(li);
-    bookList.appendChild(line);
-  });
-}
-
-render();
-
-function saveBook() {
-  localStorage.setItem('book', JSON.stringify(books));
-}
-
-function addBook(event) {
-  event.preventDefault();
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  books.push({ title, author });
-  render();
-  bookForm.reset();
-  saveBook();
-}
-
-function remove(event) {
-  if (event.target.tagName === 'BUTTON') {
-    const index = event.target.id;
-    books.splice(index, 1);
-    render();
+class Library {
+  constructor(){
+      if (localStorage.length>0){
+          this.books = JSON.parse(localStorage.getItem("books"));
+      }
+      else {
+          this.books = [];
+      }
   }
-  saveBook();
+
+  storeLocalStorage(){
+      localStorage.setItem("books", JSON.stringify(this.books));
+  }
+  
+  addBook (){
+      title = document.querySelector('#title').value;
+      author = document.querySelector('#author').value;
+  
+      const book = {
+          title: title,
+          author: author
+      }
+      if(this.books){
+          this.books.push(book);
+      }
+  }
+  
+  loadLocalStorage(){
+     if (localStorage.length>0){
+          this.books = JSON.parse(localStorage.getItem("books"));
+      }
+  }
+  
+  removeList(){
+      while (bookList.firstChild) {
+          bookList.removeChild(bookList.firstChild);
+      }
+  }
+  
+  displayData(){
+      this.removeList();
+        if(this.books.length>0){
+          for(let i=0; i<this.books.length; i++){
+              let title = this.books[i].title;
+              let author = this.books[i].author;
+              let li = document.createElement('li');
+              li.innerHTML = `${title}<br>${author}`;
+              bookList.appendChild(li);
+              let btn = document.createElement('button');
+              btn.classList.add('delete-btn');
+              btn.id = i;
+              btn.innerHTML = 'Remove';
+              bookList.appendChild(btn);
+              let hr = document.createElement('hr');
+              hr.classList.add('booklist-hr');
+              bookList.appendChild(hr);
+          }
+        }
+      else {
+          let h3 = document.createElement('h3');
+          h3.innerHTML = `No record found`;
+          bookList.appendChild(h3);
+      }
+  }
+
+  deleteItem(ev){
+      if(ev.target.classList == 'delete-btn'){
+          let id = ev.target.id;
+          this.books.splice(id, 1);
+          this.storeLocalStorage();
+          this.loadLocalStorage();
+          this.displayData();
+      }
+  }
 }
 
-bookForm.addEventListener('submit', addBook);
-bookList.addEventListener('click', remove);
+const library = new Library();
+
+const form = document.querySelector('#book-form');
+const catalog = document.querySelector('.book-catalog');
+const h1 = document.querySelector('h1');
+const bookList = document.querySelector('.book-list');
+let title, author;
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  library.addBook();
+  library.storeLocalStorage();
+  library.displayData();
+  form.reset();
+});
+
+window.addEventListener('load', () => {
+  library.loadLocalStorage();
+  library.displayData();
+});
+
+bookList.addEventListener('click', (ev) => {
+  library.deleteItem(ev);
+});
