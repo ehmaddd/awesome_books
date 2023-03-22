a900459cc7451dfce5bb3c10633f137b30e762b0
Ahmad <ehmaddd@gmail.com>
2023-03-22 17:46:26 +0500

    Add updated files

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-// class Book {
-//   constructor(title, author) {
-//     this.title = title;
-//     this.author = author;
-//   }
-// }
-
 class BookShelf {
   constructor() {
-    this.books = JSON.parse(localStorage.getItem('book')) || [{title: 'title 1', author: 'author 2'}, {title: 'title 1', author: 'author 2'}]
+    this.books = JSON.parse(localStorage.getItem('book')) || [{ title: 'title 1', author: 'author 2' }, { title: 'title 1', author: 'author 2' }];
   }
 
   add(title, author) {
@@ -23,7 +16,7 @@ class BookShelf {
   render() {
     const bookList = document.querySelector('#book-list');
     bookList.innerHTML = '';
-    for (let i = 0; i < this.books.length; i++) {
+    for (let i = 0; i < this.books.length; i += 1) {
       const book = this.books[i];
       const li = document.createElement('li');
       li.innerHTML = `${book.title} by ${book.author}`;
@@ -33,19 +26,19 @@ class BookShelf {
       deleteButton.addEventListener('click', () => {
         this.remove(book);
         this.render();
-      })
+      });
       li.appendChild(deleteButton);
       bookList.appendChild(li);
 
       if (i % 2 === 0) {
-        li.style.backgroundColor = '#e9e9e9'
+        li.style.backgroundColor = '#e9e9e9';
       }
     }
   }
 }
 
 const myBookShelf = new BookShelf();
-const addBook = document.querySelector('#add-book')
+const addBook = document.querySelector('#add-book');
 addBook.addEventListener('click', (event) => {
   event.preventDefault();
   const titleInput = document.querySelector('#title');
@@ -63,51 +56,30 @@ addBook.addEventListener('click', (event) => {
 myBookShelf.render();
 
 const sections = document.getElementsByTagName('section');
-for (let i = 1; i < sections.length; i++) {
+for (let i = 1; i < sections.length; i += 1) {
   sections[i].style.display = 'none';
 }
 
 const navLinks = document.querySelectorAll('nav a');
 navLinks[0].classList.add('active');
 
-for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener('click', function(event) {
+for (let i = 0; i < navLinks.length; i += 1) {
+  navLinks[i].addEventListener('click', function update(event) {
     event.preventDefault();
-    for (let j = 0; j < sections.length; j++) {
+    for (let j = 0; j < sections.length; j += 1) {
       sections[j].style.display = 'none';
     }
     const target = this.getAttribute('href').substr(1);
     document.getElementById(target).style.display = 'block';
 
-    for (let k = 0; k < navLinks.length; k++) {
+    for (let k = 0; k < navLinks.length; k += 1) {
       navLinks[k].classList.remove('active');
     }
     this.classList.add('active');
   });
 }
 
-sections[0].style.display = 'block   ';
-
-
-
-
-// const sections = document.getElementsByTagName('section');
-// for (let i = 1; i < sections.length; i++) {
-//   sections[i].style.display = 'none';
-// }
-
-// const navLinks = document.querySelectorAll('nav a');
-// for (let i = 0; i < navLinks.length; i++) {
-//   navLinks[i].addEventListener('click', function(event) {
-//     event.preventDefault();
-//     for (let j = 0; j < sections.length; j++) {
-//       sections[j].style.display = 'none';
-//     }
-
-//     const target = this.getAttribute('href').substr(1);
-//     document.getElementById(target).style.display = 'block';
-//   });
-// }
+sections[0].style.display = 'block';
 
 function displayDateTime() {
   const currentDate = new Date();
